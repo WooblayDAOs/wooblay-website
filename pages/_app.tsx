873a004f59f67c030f6dfdaa375044889acd3a16
RealWooblay <jackaustinc66@gmail.com>
2024-08-28 12:01:59 +1000

    cleanup and remove '

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import type { AppProps } from "next/app";
+
 import { NextUIProvider } from "@nextui-org/system";
+
 import { useRouter } from "next/router";
 
 import { fontSans, fontMono } from "@/config/fonts";
+
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
